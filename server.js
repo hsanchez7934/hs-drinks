2a6e0b9b74480d912e33ea22e61ddb382e972112
Hector Sanchez <hsanchez7934@gmail.com>
2018-01-05 14:43:31 -0700

    Change path of html file to server inside of server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.set('port', process.env.PORT || 3002);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/drinks-frontend/public/index.html'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.locals.title = 'Drinks';
