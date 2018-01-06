658fec18489d27b212f6cec2d6adb7f75812863e
Hector Sanchez <hsanchez7934@gmail.com>
2018-01-05 17:35:52 -0700

    Add heroku-postbuild script to server package.json

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.set('port', process.env.PORT || 3002);
-app.use(express.static('/drinks-frontend/build/index.html'));
+app.use(express.static('/drinks-frontend/build/'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.locals.title = 'Drinks';
