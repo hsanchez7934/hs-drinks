94e4c5243c3748acf33bc3d2ade93072184bc1ff
Hector Sanchez <hsanchez7934@gmail.com>
2018-01-10 10:41:40 -0700

    add cachdirectories to package.json

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.locals.title = 'Drinks';
 
+app.get('/api', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send('{"message":"Hello from the custom server!"}');
+});
+
 app.get('*', function(request, response) {
   response.sendFile(
     path.resolve(__dirname, './drinks-frontend/build', 'index.html')
